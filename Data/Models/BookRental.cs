7f5cbaded39b61f3230666759b9f5379878567c4
Dawid Nowotny <dawidnowotny@o2.pl>
2024-10-18 16:19:02 +0200

    Added database models

---
+++ Data/Models/BookRental.cs
@@ -0,0 +1,14 @@
+﻿namespace library_api.Models
+{
+	public class BookRental
+	{
+		public int Id { get; set; }
+		public int BookId { get; set; }
+		public int UserId { get; set; }
+		public DateTime RentalDate { get; set; }
+		public DateTime? ReturnDate { get; set; }
+		public bool IsReturned { get; set; }
+		public User User { get; set; }
+		public Book Book { get; set; }
+	}
+}
---
+++ Data/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace library_api.Models
+{
+	public enum UserRole
+	{
+		Admin,
+		Librarian,
+		User,
+	}
+
+	public class User
+	{
+		public int Id { get; set; }
+		public string Username { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public UserRole Role { get; set; } = UserRole.User;
+		public ICollection<BookRental> BookRental { get; set; }
+	}
+}
