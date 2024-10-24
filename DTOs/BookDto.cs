9c1d7b22028cf99d0c9257a3f3774f1e3c024f20
Dawid Nowotny <dawidnowotny@o2.pl>
2024-10-24 11:01:32 +0200

    Added custom date formatting for book retrieval responses

--- DTOs/BookDto.cs
+++ DTOs/BookDto.cs
@@ -1,10 +1,14 @@
-﻿namespace library_api.DTOs
+﻿using library_api.Converters;
+using System.Text.Json.Serialization;
+
+namespace library_api.DTOs
 {
 	public class BookDto
 	{
 		public string Title { get; set; }
 		public string Author { get; set; }
 		public string ISBN { get; set; }
+		[JsonConverter(typeof(DateOnlyConverter))]
 		public DateTime PublishedDate { get; set; }
 		public int CopiesAvailable { get; set; }
 	}
